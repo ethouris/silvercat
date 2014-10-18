b3198a869f22ff65f74fbb0d3f757692493f6f0c
Sektor van Skijlen <ethouris@gmail.com>
2014-10-18 12:44:09 +0200

    Made ag genrules generate Makefile.tcl file directly. Some cosmetics

--- bin/agv.p.builtin-profiles.tcl
+++ bin/agv.p.builtin-profiles.tcl
@@ -4,15 +4,25 @@ set profiles {
 	# {This is only informative - explains the structure of the profiles.}
 	structure {
 
-		# { Lang entry named "general" defines things that are common
-			# for all languages. }
+		# { Lang entry named "default" defines things that are common
+		# for all languages. }
 		lang {
 			compile "Command to produce .o file from source file"
 			compile_oflag "Usually -o"
 			link "Command to produce executable file"
 			link_oflag "Usually -o"
 			linkdl "(optional) Command to produce dynamic library file (defaults to link)"
-			gendep "Command to generate dependencies (produces a list of all files that can be extracted of a single source file)"
+
+			# { The format of the output data for gendep should be as it's generated for Makefile, that is:
+			#	 - The first word in the line is the target name followed by a colon
+			#    - Then space-separated filenames of ingredients in one line
+			#    - Lines may be extended by backslash
+			# If you want to make a custom dependency generator, just note that the first word is dropped
+			# (output file is already known), so it need not be ended by a colon and it needn't be anything
+			# meaningful at all. Just must be present and consist of non-white characters.
+			# }
+			gendep "Command to generate dependencies
+					(produces a list of all files that can be extracted of a single source file)"
 			preproc "Command to run preprocessor (optional, can be empty if a language doesn't use one)"
 			cflags "Flags passed always to compile command (compile, gendep, preproc)"
 			ldflags "Flags passed to link command"
@@ -35,6 +45,7 @@ set profiles {
 			# the 64-bit libraries are installed in lib64,
 			# while lib is only for 32-bit libraries }
 			installdir:lib {$prefix/lib}
+			installdir:data {$prefix/share}
 		}
 	}
 
